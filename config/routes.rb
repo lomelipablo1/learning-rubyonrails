ada30fa88a9c6458ba7a6e2f51eee1f22e070398
Pablo Lomeli <lomelipablo1@gmail.com>
2024-10-02 20:24:07 -0700

    fixing route file again

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # Defines the root path route ("/")
+  root "articles#index"
   get "/articles", to: "articles#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,7 +11,5 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  root "articles#index"
+  
 end
